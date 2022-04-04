37f3dc151cfbac15785302c184c400e7cc6cb98a
XereJay <xereyou@gmail.com>
2022-04-04 17:12:28 +0200

    solved sudoku

--- sudoku.py
+++ sudoku.py
@@ -1,9 +1,20 @@
+from typing import List
+
+
 def main():
     assert is_correct([1, 2, 8, 9, 3, 5, 6, 7, 4]) == True
     assert is_correct([1, 2, 8, 9, 3, 5, 7, 4]) == False
     assert is_correct([1, 1, 2, 8, 9, 3, 5, 7, 4]) == False
     assert is_correct([]) == False
 
+def is_correct(sudoku_list : List[int]):
+    sudoku_correct = {1,2,3,4,5,6,7,8,9}
+    sudoku_set = set(sudoku_list)
+    
+    return True if sudoku_correct.difference(sudoku_set) == set() else False
+
+
 
 if __name__ == "__main__":
-    main()
+    print(main())
+    
